38dd4f3b4aa744e78fd0b735b87dbb2552b51bb9
Tom MARX <tom.marx@hotmail.fr>
2020-09-22 16:16:31 +0200

    Tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,15 +5,11 @@
 
 int main(void)
 {
-	// test_vector();
-	// test_list();
-	// test_map();
-	// test_stack();
-	// test_queue();
-
-	ft::Vector<int> a((size_t)10, 3);
-	ft::Vector<int> b = a;
-	std::cout << (a.capacity() == b.capacity()) << std::endl;
+	test_vector();
+	test_list();
+	test_map();
+	test_stack();
+	test_queue();
 
 	return (0);
 }
